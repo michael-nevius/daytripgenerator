082c5881f4ed7bf174a1808c52c8f7b154df1299
michael-nevius <michael.a.nevius@gmail.com>
2022-03-29 12:19:02 -0400

    Still working on it but it's coming along

--- day_trip_generator.py
+++ day_trip_generator.py
@@ -12,40 +12,42 @@ def generateRandomOption(someArray):
 def placegeneration(place):
     place = generateRandomOption(dest)
     user_input = input(f'We have selected {place} does this work for you? y/n: ')
-    while user_input != 'y':
+    while user_input is 'n':
           print(f'We have selected {place} does this work for you? y/n: ')
-    print(f'Lets move onto your restaraunt selection!')
+def  foodgeneration(food):
+     food = generateRandomOption(restaraunt)
+     user_input = input(f'We have selected {food} does this work for you? y/n: ') 
+     return user_input
+def  transpgeneration(vehicle):
+     vehicle = generateRandomOption(transport)
+     user_input = input(f'We have selected {vehicle} does this work for you? y/n: ')
+     return user_input
+def  entertaingeneration(fun):
+     fun = generateRandomOption(entertain)
+     user_input = input(f'We have selected {fun} does this work for you? y/n: ')
+     return user_input
+def get_user_choice(some_list):
+    user_choice = "n"
+    while user_choice.lower() == "n":
+            random_selection = generateRandomOption(some_list)
+            user_choice = input(f"We have selected {random_selection}. Do you like this choice? Enter y/n :")
+            if user_choice.lower() == 'n':
+                print("Sorry I guess that wasn't good enough how about now")
+                some_list.remove(random_selection)
+            else:
+                print(f"Finally you have chosen  {random_selection}.")
+    return random_selection
 
-# def  foodgeneration(food):
-#      food = generateRandomOption(restaraunt)
-#      user_input = input(f'We have selected {food} does this work for you? y/n: ') 
-#      return user_input
-# def  transpgeneration(vehicle):
-#      vehicle = generateRandomOption(transport)
-#      user_input = input(f'We have selected {vehicle} does this work for you? y/n: ')
-#      return user_input
-# def  entertaingeneration(fun):
-#      fun = generateRandomOption(entertain)
-#      user_input = input(f'We have selected {fun} does this work for you? y/n: ')
-#      return user_input
-
-
-# place = placegeneration(dest)
-# user_input = input(f'We have selected {place} does this work for you? y/n: ')
-
-# food = generateRandomOption(restaraunt)
-# user_input = input(f'We have selected {food} does this work for you? y/n: ')
-       
-# vehicle = transpgeneration(transport)
-# user_input = input(f'We have selected {vehicle} does this work for you? y/n: ')
-
-# fun = generateRandomOption(entertain)
-# user_input = input(f'We have selected {fun} does this work for you? y/n: ')
-
-
-# print(f'Thank you for using the Day Time Trip Generator {name}! We hope you enjoy the day! Please come back and see use again!')
-# print('Here is your Itinerary.')
-# print(f'Destination: {place}')
-# print(f'Restaraunt: {food}')
-# print(f'Transportation: {vehicle}')
-# print(f'Entertainment: {fun}') 
+final_destination = get_user_choice (dest)
+restaraunt = foodgeneration(final_destination)
+final_restaurant = get_user_choice (restaraunt)
+modes_of_transportation = transpgeneration(final_destination)
+final_mode_of_transportation = get_user_choice (transport)
+forms_of_entertainment = entertaingeneration(final_destination)
+final_form_of_entertainment = get_user_choice(entertain)
+print(f"""Congratulations! {name} Your day trip booking is complete.
+You have chosen to travel to {final_destination}.
+You have chosen to dine at {final_restaurant}.
+You have chosen to use a/an {final_mode_of_transportation} to get around.
+You have chosen to {final_form_of_entertainment}.
+Have a great time!!""")
